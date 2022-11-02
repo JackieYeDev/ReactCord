60d644ce0768f8f32a3244482d514dcb912f9499
Jackie <jye0325@gmail.com>
2022-11-02 11:03:16 -0400

    Add create and destroy to Channel controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,23 @@
 class ChannelsController < ApplicationController
+  def create
+    user_id = session[:user_id]
+    if user_id.nil?
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    else
+      channel = Channel.create!(:name=>params[:name], :user_id=> user_id)
+      subscription = Subscription.create!(:channel_id=>channel.id, :user_id=> user_id)
+      render json: channel, status: :created
+    end
+  end
+
+  def destroy
+    user_id = session[:user_id]
+    channel = Channel.find(params[:channel_id])
+    if channel.user_id == user_id
+      channel.destroy
+      head :no_content
+    else
+      render json: { error: "Unauthorized. You are not the owner of this channel." }, status: :unauthorized
+    end
+  end
 end
