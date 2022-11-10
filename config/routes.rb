4edf81847747a91740f9ffbf8e9e49edd009be09
Jackie <jye0325@gmail.com>
2022-11-09 19:42:08 -0500

    Add routes for User, Channels, and Messages model and Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  
   resources :messages
   resources :subscriptions
-  resources :channels
   resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  mount ActionCable.server => '/cable'
+  resources :channels do
+    resources :messages, only: [:index, :create]
+  end
 end
